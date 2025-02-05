85495ceb196275463530be1f5d35978473c10a38
Ben Parsons <144669631+Parsnip-69@users.noreply.github.com>
2025-02-05 14:57:55 +0000

    Add documents and archive pages; implement routing and enhance styles for document display

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def index():
 def trustee():
     return render_template('trustee.html')
 
+@app.route('/documents')
+def documents():
+    return render_template('documents.html')
+
 @app.route('/squirrels')
 def squirrels():
     return render_template('squirrels.html')
@@ -26,6 +30,10 @@ def cubs():
 @app.route('/scouts')   
 def scouts():
     return render_template('scouts.html')
+
+@app.route('/archive')
+def archive():
+    return render_template('archive.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
